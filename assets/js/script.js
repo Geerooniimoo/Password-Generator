657d87d25e7c4131eef75969b5bf9a1b37a3d88a
Geronimo Perez <geronimo.perez4@gmail.com>
2023-01-12 23:11:49 -0500

    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,26 @@
-var numChar='0123456789';
-var lowerChar='abcdefghijklmnopqrstuvwxyz';
-var upperChar='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var specialChar='@%+!#$%^&*()_-,<.>/?[{]}|:;';
-var pwBtn=document.querySelector("#generate");
-var pwDisplay=document.querySelector("#password");
 
-pwBtn.addEventListener('click',passwordGenerator);
+const passGen = () => {
+        let len=prompt('From 8 - 128, how long should password be?');
+        if (len<8 || len>128) return alert('Password length does not match criteria.  Please try again.')
 
-// ========================MAIN=========================================
-function passwordGenerator() {
-    var len=chPwLen();
-    var spec=specFunc();
-    passGen(len,spec);
+        let num=confirm('Should password have numbers?');
+        let lower=confirm('Should password have lower case letters?');
+        let upper=confirm('Should password have upper case letters?');
+        let spec=confirm('Should password have special characters?');
+        
+        let temp='';
+        if (num) temp+='0123456789';
+        if (lower) temp+='abcdefghijklmnopqrstuvwxyz';
+        if (upper) temp+='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (spec) temp+='@%+!#$%^&*()_-<>/?[{]}|:;';
+            
+        let output='';
+        for (let i = 0; i < len; i++) {
+            output+=temp[Math.floor(Math.random()*temp.length)]
+        };
+
+        password.innerText=output;
 };
-// ========================LENGTH=======================================
-function chPwLen() {
-    var pwLen=prompt('From 8 - 128, how long should password be?');
-    if (!(pwLen>=8&pwLen<=128)) {
-        alert('Wrong range.  Please try again.');
-        chPwLen();
-    }
-    return pwLen;
-};
-// ========================SPECIFICATIONS==============================
-function specFunc() {
-    var specs='';
-    var pwHasNum=confirm('Should password have numbers?');
-    var pwHasLower=confirm('Should password have lower case letters?');
-    var pwHasUpper=confirm('Should password have upper case letters?');
-    var pwHasSpec=confirm('Should password have special characters?');
-    
-    if (pwHasNum) {specs+=numChar};
-    if (pwHasLower) {specs+=lowerChar};
-    if (pwHasUpper) {specs+=upperChar};
-    if (pwHasSpec) {specs+=specialChar};
-    
-    return specs;
-};
-// ========================GENERATOR===================================
-function passGen(len, spec) {
-    var password='';
-    for (let i = 0; i < len; i++) {
-        password+=spec[Math.floor(Math.random()*spec.length)]
-    };
-    pwDisplay.value = password;
-}
+
+generate.addEventListener('click',passGen);
+
