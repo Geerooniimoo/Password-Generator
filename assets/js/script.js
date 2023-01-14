bf780d5435176d0628570ffd69f080958ba1099a
Geronimo Perez <geronimo.perez4@gmail.com>
2023-01-14 16:30:04 -0500

    GenPass is fully functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,57 @@
-let store;
 
 document.querySelector('.card').innerHTML += `
 <section style="display:none;margin-top:20px;" id="hanSav"><button style="background:yellow;width:50%;" onclick="clearPass()">Clear</button><button style="background:hsl(360,91%,36%);width:50%;height:30px;color:white;" onclick="delPass()">Delete</button></section>
 `;
 
+document.querySelector('.card-header').innerHTML += `
+<section style="display:flex;flex-wrap:wrap;justify-content:space-around;margin-top:20px;" id="history"></section>
+`;
+
+let store;
+let history = document.getElementById('history');
+
+const renderStore = () => {
+    if (store.length) {
+        history.innerHTML = '<h2 style="width:100%;text-align:center;">Saved Passowrds</h2>';
+        store.forEach(obj => {
+            Object.keys(obj).forEach(key => {
+                history.innerHTML += `
+                <button style="background:teal;color:white;margin:5px;border-radius:8px;border: 4px solid black;padding:5px;" onclick="hanPass()">${key}</button>`
+            });
+        });
+    } else {
+        history.innerHTML = '';
+    };
+};
+
 const clearPass = () => {
     password.innerText = '';
+    document.querySelector('.card h2').innerText = 'Generate a Password';
     document.getElementById('hanSav').style.display = 'none';
-    generate.style = 'display:block;margin:0 auto;';
+    generate.style = 'display:block;margin:16px auto auto;';
+};
+
+const delPass = () => {
+    let name = document.querySelector('.card h2').innerText;
+    store = store.filter(obj => Object.keys(obj) != name);
+    localStorage.passwords = JSON.stringify(store);
+    clearPass();
+    renderStore();
 };
 
 const hanPass = () => this.addEventListener('click', ({target:{innerText:name}}) => {
     password.innerText = (store.filter(obj=>obj[name])[0])[name];
+    document.querySelector('.card h2').innerText = name;
     generate.style.display = 'none';
     hanSav.style.display = 'flex';
-
 });
 
 
 (async () => {
-    document.querySelector('.card-header').innerHTML += `
-        <section style="display:flex;flex-wrap:wrap;justify-content:space-around;margin-top:20px;" id="history"></section>
-    `;
-
     let hanSav = document.getElementById('hanSav');
 
     store = localStorage.passwords ?
         await eval(localStorage.passwords) : [];
-    let history = document.getElementById('history');
-
-
-    const renderStore = () => {
-
-        if (store.length) {
-            history.innerHTML = '<h2 style="width:100%;text-align:center;">Saved Passowrds</h2>';
-            store.forEach(obj => {
-                Object.keys(obj).forEach(key => {
-                    history.innerHTML += `
-                    <button style="background:teal;color:white;margin:5px;border-radius:8px;padding:5px;" onclick="hanPass()">${key}</button>`
-                });
-            });
-        };
-    };
 
     renderStore();
 
@@ -65,11 +75,12 @@ const hanPass = () => this.addEventListener('click', ({target:{innerText:name}})
             output += temp[Math.floor(Math.random() * temp.length)]
         };
 
-        password.innerText = output;
+        let name = prompt('Password\'s name?').toUpperCase();
+        document.querySelector('.card h2').innerText = name;
         generate.style.display = 'none';
         hanSav.style.display = 'flex';
-
-        let name = prompt('Password\'s name?').toUpperCase();
+        password.innerText = output;
+        
         store.push({ [name]: output });
 
         localStorage.passwords = JSON.stringify(store);
